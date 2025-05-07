46d0913c00e9f2187ceae4982f6eab815c960c79
Zach Yeap <zyeap@juniper.net>
2025-05-07 14:36:43 -0700

    //

--- init.lua
+++ init.lua
@@ -675,7 +675,7 @@ require('lazy').setup({
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         -- ts_ls = {},
         --
-
+        --
         lua_ls = {
           -- cmd = { ... },
           -- filetypes = { ... },
@@ -708,8 +708,8 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
-        'ruff',
         'pyright',
+        'black',
         'gopls',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
@@ -763,7 +763,7 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
-        python = { 'ruff format' },
+        python = { 'black' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
