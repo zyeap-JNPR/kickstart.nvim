5b0dfe9e76f07703a18eb1f5053c5398e5702071
Zach Yeap <zyeap@juniper.net>
2025-05-07 14:19:34 -0700

    completion mapping

--- init.lua
+++ init.lua
@@ -831,7 +831,12 @@ require('lazy').setup({
         -- <c-k>: Toggle signature help
         --
         -- See :h blink-cmp-config-keymap for defining your own keymap
-        preset = 'default',
+        preset = 'none',
+        ['<CR>'] = { 'accept', 'fallback' },
+        ['<tab>'] = { 'select_next', 'fallback' },
+        ['<s-tab>'] = { 'select_prev', 'fallback' },
+        ['<Down>'] = { 'select_next', 'fallback' },
+        ['<Up>'] = { 'select_prev', 'fallback' },
 
         -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
         --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
